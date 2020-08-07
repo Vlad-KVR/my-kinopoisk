a0b8e40e6713a69feba3e975d8c5cec8aaf2b905
Vladislav Kovalev <max2433186@gmail.com>
2020-08-07 16:35:19 +0300

    Second commit

--- script.js
+++ script.js
@@ -2,42 +2,41 @@
 
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 const apiSearch = (e) => {
     e.preventDefault();
     const searchText = document.querySelector('.form-control').value,
         server = 'https://api.themoviedb.org/3/search/multi?api_key=283f38ee982e94c550f5c940d9745ad5&language=ru&query=' + searchText;
-    requestApi('GET', server);
+    
+    movie.innerHTML = 'Загрузка';
+
+    fetch(server)
+    .then(val => {
+        return val.status === 200 ? val.json() : 
+            Promise.reject(val);
+    })
+    .then(output => {
+        let inner = '';
+    
+        output.results.forEach(element => {
+            let nameItem = element.name || element.title;
+
+            inner += `<div class="col-12 col-md-4 col-xl-3 item">
+            <img src="${urlPoster + element.poster_path}" alt="${nameItem}">
+            <h5>${nameItem}</h5>
+            </div>`;
+        });
+
+        movie.innerHTML = inner;
+    })
+    .catch(reason => {
+        movie.innerHTML = "Упс, что то пошло не так!";
+        console.error('error: ' + reason.status);
+    });
+
     
 };
 
 searchForm.addEventListener('submit', apiSearch);
 
-
-const requestApi = (method, url) => {
-
-    const request = new XMLHttpRequest();
-    request.open(method, url);
-    request.send();
-
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState !== 4) return;
-
-        if(request.status !== 200) {
-            console.log('error: ' + request.status);
-            return;
-        }
-
-        const output = JSON.parse(request.responseText);
-        
-        let inner = '';
-
-        output.results.forEach(element => {
-            let nameItem = element.name || element.title;
-            inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem}</div>`;
-        });
-
-        movie.innerHTML = inner;
-
-    });
-};
